40a24a286be4b9a27371b2dfdcdf517fe24bd928
xpycteam88 <sicuno@mail.ru>
2023-11-09 23:34:20 +0300

    Task done

--- src/homework12/Author.java
+++ src/homework12/Author.java
@@ -1,5 +1,7 @@
 package homework12;
 
+import java.util.Objects;
+
 public class Author {
     private String firstNameAuthor;
     private String lastNameAuthor;
@@ -12,8 +14,25 @@ public class Author {
     public String getFirstNameAuthor() {
         return firstNameAuthor;
     }
-
     public String getLastNameAuthor() {
         return lastNameAuthor;
     }
+    @Override
+    public String toString() {
+        return "Автор: " + firstNameAuthor + ' ' + lastNameAuthor + ';';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(firstNameAuthor, author.firstNameAuthor) && Objects.equals(lastNameAuthor, author.lastNameAuthor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstNameAuthor, lastNameAuthor);
+    }
+
 }
--- src/homework12/Book.java
+++ src/homework12/Book.java
@@ -1,29 +1,44 @@
 package homework12;
 
+import java.util.Objects;
+
 public class Book {
     private final String title;
     private Author author;
     private int publishingYear;
-
     public Book(String title, Author author, int publishingYear) {
         this.title = title;
         this.author = author;
         this.publishingYear = publishingYear;
     }
-
     public String getTitle() {
         return title;
     }
-
     public Author getAuthor() {
         return author;
     }
-
     public int getPublishingYear() {
         return publishingYear;
     }
-
     public void setPublishingYear(int publishingYear) {
         this.publishingYear = publishingYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Книга: " + title + "; " + author + " год публикации: " + publishingYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author);
     }
 }
