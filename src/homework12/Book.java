3cec27e33da78979aed91f32ff0a8313e8213f7e
xpycteam88 <sicuno@mail.ru>
2023-11-10 15:46:12 +0300

    Edited the method equals in class Book

--- src/homework12/Book.java
+++ src/homework12/Book.java
@@ -34,11 +34,12 @@ public class Book {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Book book = (Book) o;
-        return Objects.equals(title, book.title) && Objects.equals(author, book.author);
+        return publishingYear == book.publishingYear && Objects.equals(title, book.title) && Objects.equals(author, book.author);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, author);
+        return Objects.hash(title, author, publishingYear);
     }
 }
+
