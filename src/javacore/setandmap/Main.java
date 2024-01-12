ef35a7c203d0fca81c83d607b6092ce0f95eb79c
xpycteam88 <sicuno@mail.ru>
2024-01-12 23:00:59 +0300

    Tasks done!

--- src/javacore/setandmap/Main.java
+++ src/javacore/setandmap/Main.java
@@ -1,7 +1,18 @@
 package javacore.setandmap;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    private static final Listservice listservice = new Listservice();
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    public static final List<String> strings = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+
+    public static void main(String[] args) {
+        System.out.println(nums);
+        listservice.task1(nums);
+        listservice.task2(nums);
+        listservice.task3(strings);
+        listservice.task4(strings);
     }
 }
